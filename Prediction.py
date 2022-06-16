ea9e3e0b0e60a1370c4ac0d981baae2f46bf913a
GBasavaraju <gaba00002@stud.uni-saarland.de>
2022-06-16 21:06:13 +0200

    House price prediction

---
+++ Prediction.py
@@ -0,0 +1,180 @@
+# IMPORTING PACKAGES
+
+import pandas as pd # data processing
+import numpy as np # working with arrays
+import matplotlib.pyplot as plt # visualization
+import seaborn as sb # visualization
+
+from sklearn.model_selection import train_test_split # data split
+
+from sklearn.linear_model import LinearRegression # OLS algorithm
+from sklearn.linear_model import Ridge # Ridge algorithm
+from sklearn.linear_model import Lasso # Lasso algorithm
+
+from sklearn.metrics import explained_variance_score as evs # evaluation metric
+from sklearn.metrics import r2_score as r2 # evaluation metric
+
+df = pd.read_csv('D:\MSc-Saarland\Projects\House Price Prediction\House_Data.csv')
+df.set_index('Id', inplace = True)
+
+df.head(5)
+
+df.dropna(inplace = True)
+
+# print(df.isnull().sum())
+df.describe()
+#print(df.dtypes)
+
+df['MasVnrArea'] = pd.to_numeric(df['MasVnrArea'], errors = 'coerce')
+df['MasVnrArea'] = df['MasVnrArea'].astype('int64')
+
+#print(df.dtypes)
+
+#ScatterPlot
+def scatter_df(y_var):
+    scatter_df = df.drop(y_var, axis = 1)
+    i = df.columns
+    
+    plot1 = sb.scatterplot(i[0], y_var, data = df, color = 'orange', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[0]), fontsize = 16)
+    plt.xlabel('{}'.format(i[0]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter1.png')
+    plt.show()
+    
+    plot2 = sb.scatterplot(i[1], y_var, data = df, color = 'yellow', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[1]), fontsize = 16)
+    plt.xlabel('{}'.format(i[1]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter2.png')
+    plt.show()
+    
+    plot3 = sb.scatterplot(i[2], y_var, data = df, color = 'green', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[2]), fontsize = 16)
+    plt.xlabel('{}'.format(i[2]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter3.png')
+    plt.show()
+    
+    plot4 = sb.scatterplot(i[3], y_var, data = df, color = 'blue', edgecolor = 'black', s = 150)
+    plt.title('{} / Sale Price'.format(i[3]), fontsize = 16)
+    plt.xlabel('{}'.format(i[3]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter4.png')
+    plt.show()
+    
+    plot5 = sb.scatterplot(i[4], y_var, data = df, color = 'red', edgecolor = 'black', s = 150)
+    plt.title('{} / Sale Price'.format(i[4]), fontsize = 16)
+    plt.xlabel('{}'.format(i[4]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter5.png')
+    plt.show()
+    
+    plot6 = sb.scatterplot(i[5], y_var, data = df, color = 'violet', edgecolor = 'black', s = 150)
+    plt.title('{} / Sale Price'.format(i[5]), fontsize = 16)
+    plt.xlabel('{}'.format(i[5]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter6.png')
+    plt.show()
+    
+    plot7 = sb.scatterplot(i[6], y_var, data = df, color = 'brown', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[6]), fontsize = 16)
+    plt.xlabel('{}'.format(i[6]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter7.png')
+    plt.show()
+    
+    plot8 = sb.scatterplot(i[7], y_var, data = df, color = 'gold', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[7]), fontsize = 16)
+    plt.xlabel('{}'.format(i[7]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter8.png')
+    plt.show()
+    
+    plot9 = sb.scatterplot(i[8], y_var, data = df, color = 'r', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[8]), fontsize = 16)
+    plt.xlabel('{}'.format(i[8]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter9.png')
+    plt.show()
+    
+    plot10 = sb.scatterplot(i[9], y_var, data = df, color = 'pink', edgecolor = 'b', s = 150)
+    plt.title('{} / Sale Price'.format(i[9]), fontsize = 16)
+    plt.xlabel('{}'.format(i[9]), fontsize = 14)
+    plt.ylabel('Sale Price', fontsize = 14)
+    plt.xticks(fontsize = 12)
+    plt.yticks(fontsize = 12)
+    plt.savefig('scatter10.png')
+    plt.show()
+    
+scatter_df('SalePrice')
+
+#HeatMap
+sb.heatmap(df.corr(), annot = True, cmap = 'magma')
+plt.savefig('heatmap.png')
+plt.show()
+
+# 3. Distribution plot
+
+sb.distplot(df['SalePrice'], color = 'r')
+plt.title('Sale Price Distribution', fontsize = 16)
+plt.xlabel('Sale Price', fontsize = 14)
+plt.ylabel('Frequency', fontsize = 14)
+plt.xticks(fontsize = 12)
+plt.yticks(fontsize = 12)
+plt.savefig('distplot.png')
+plt.show()
+
+
+#Split the Data
+
+X_var = df[['LotArea', 'MasVnrArea', 'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF']].values
+y_var = df['SalePrice'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X_var, y_var, test_size = 0.2, random_state = 0)
+
+#print('X_train samples : ', X_train[0:5])
+#print('X_test samples : ', X_test[0:5])
+#print('y_train samples : ', y_train[0:5])
+#print('y_test samples : ', y_test[0:5])
+
+#Modelling
+
+#LinearRegression
+
+ols = LinearRegression()
+ols.fit(X_train, y_train)
+ols_yhat = ols.predict(X_test)
+
+# Ridge
+
+ridge = Ridge(alpha = 0.5)
+ridge.fit(X_train, y_train)
+ridge_yhat = ridge.predict(X_test)
+
+#Evaluate
+#print('Explained Variance Score')
+print(f'Explained Variance score of Ols Model is: {evs(y_test, ols_yhat)}' )
+print(f'Explained Variance Score of Ridge model is {evs(y_test, ridge_yhat)}')
+
+#RSquared
+print(f'R-Squared of OLS model is {r2(y_test, ols_yhat)}')
+print(f'R-Squared of Ridge model is {r2(y_test, ridge_yhat)}')
